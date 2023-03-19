fb7ab993fbf1b23995f07e33f94981c7f2b7ff13
HarshithaB-GIT <107307761+HarshithaB-GIT@users.noreply.github.com>
2023-03-19 12:36:05 +0530

    Add files via upload

---
+++ bedroom.js
@@ -0,0 +1,39 @@
+status = "";
+object=[];
+function preload() {
+    img = loadImage("Bedroom.jpeg") ;
+}
+function setup(){
+    canvas = createCanvas(640,420) ;
+    canvas.center() ;
+    objectDetector = ml5.objectDetector("cocossd",modelLoaded) ;
+    document.getElementById("status").innerHTML = "Status : Dectecting objects";
+}
+function modelLoaded() {
+    console.log("Model is initialized");
+    status="true";
+    objectDetector.detect(img,gotResults);
+}
+function gotResults(error,results) {
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        object = results;
+    }
+}
+function draw(){
+    image(img,0,0,640,420);
+    if(status !="") {
+      for(i=0; i<object.length; i++){
+        document.getElementById("status").innerHTML = "Status : Detecting Objects";
+        fill("Red");
+        percent = floor(object[i].confidence * 100);
+        text(object[i].label+" "+percent+"%",object[i].x,object[i].y);
+        noFill();
+        stroke("Red") ;
+        rect(object[i].x,object[i].y,object[i].width,object[i].height);
+      }
+    }
+}
